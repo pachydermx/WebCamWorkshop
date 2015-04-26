3d92d95016e0fcb007c8c39acf5f3fc7bbbabe4a
PachydermX <renjimin@gmail.com>
2015-04-26 17:03:04 +0900

    Greyscaling

--- CamTest.java
+++ CamTest.java
@@ -21,7 +21,7 @@ public class CamTest extends JFrame{
     private CaptureDeviceInfo deviceInfo = null;
     private MediaLocator mediaLocator = null;
 	private Component component = null;
-	private JPanel vedioPanel = null;
+	private JPanel videoPanel = null;
 	
     String   deviceName   =   "vfw:Microsoft WDM Image Capture (Win32):0";
     // Creates a new instance of CameraTest 
@@ -38,9 +38,9 @@ public class CamTest extends JFrame{
 			player = Manager.createRealizedPlayer(mediaLocator);// 利用mediaLocator 获取一个player
 			component = player.getVisualComponent();
 			if (component != null){
-				vedioPanel = new JPanel();
-				vedioPanel.add(component, BorderLayout.NORTH);
-				this.add(vedioPanel);
+				videoPanel = new JPanel();
+				videoPanel.add(component, BorderLayout.NORTH);
+				this.add(videoPanel);
 				this.pack();	// 自动分配窗体大小
 				this.setResizable(false);
 				this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -51,38 +51,76 @@ public class CamTest extends JFrame{
             e.printStackTrace();
         }
     }
-    
-    public static void main(String[] args) {
-        new CamTest();
 
+    public int RGBtoGray(int rgb_value){
+        int r, g, b;
+        
+        r = (rgb_value & 0x00FF0000) / 0x00010000;
+        g = (rgb_value & 0x0000FF00) / 0x00000100;
+        b = (rgb_value & 0x000000FF);
+        
+        return (int)(r * 0.299 + g * 0.587 + b * 0.114);
+    }
+
+    public void process(){
         try {
             // picture capture
 
             // wait for camera launching
-            Thread.sleep(10000);
+            Thread.sleep(7000);
             System.out.println("captured");
             // grab a frame from cam
             FrameGrabbingControl frameGrabber = (FrameGrabbingControl)player.getControl("javax.media.control.FrameGrabbingControl");
             Buffer buf = frameGrabber.grabFrame();
             // convert frame to an buffered image
             Image img = (new BufferToImage((VideoFormat) buf.getFormat()).createImage(buf));
-            BufferedImage buffImg = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+            int imgWidth = img.getWidth(null);
+            int imgHeight = img.getHeight(null);
+            BufferedImage buffImg = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB);
             Graphics2D g = buffImg.createGraphics();
-            g.drawImage(img, null, null);
 
             /*
             // Overlay curent time on image
             g.setColor(Color.RED);
             g.setFont(new Font("Verdana", Font.BOLD, 16));
             g.drawString((new Date()).toString(), 10, 25);
-            System.out.println("Woww.. Captured Image");
             */
+
+            // pixel processer
+            /*
+            MediaTracker mt = new MediaTracker();
+            mt.addImage(img);
+            */
+            int[] pixelInput = new int[imgWidth * imgHeight];
+            int[] pixelOutput = new int[imgWidth * imgHeight];
+            PixelGrabber pg = new PixelGrabber(img, 0, 0, imgWidth, imgHeight, pixelInput, 0, imgWidth);
+
+            // grab pixel into grabber
+            pg.grabPixels();
+
+            // rgb to grey
+            int x, y, value;
+            for (y = 0; y < imgHeight; y++){
+                for (x = 0; x < imgWidth; x++){
+                    value = RGBtoGray(pixelInput[y*imgWidth + x]);
+                    pixelOutput[y*imgWidth + x] = 0xFF000000 + value * 0x00010101;
+                }
+            }
+
+            Image imgOutput = createImage(new MemoryImageSource(imgWidth, imgHeight, pixelOutput, 0, imgWidth));
+
+            g.drawImage(imgOutput, 0, 0, imgWidth, imgHeight, null, null);
 
             // save image to disk
             ImageIO.write(buffImg, "png", new File("c:/demo.jpg"));
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
+    }
+    
+    public static void main(String[] args) {
+        CamTest ct = new CamTest();
+        ct.process();
     }
 }
